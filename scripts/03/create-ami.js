d8eebd801e490d81d4df48c7f25cc56c057fbfb0
alilinykh <a.f.ilinykh@gmail.com>
2022-01-28 16:12:59 -0500

    HBFL-3 update manage ec2 script

--- scripts/03/create-ami.js
+++ scripts/03/create-ami.js
@@ -1,14 +1,17 @@
 // Imports
-// TODO: Import the ec2 client
-
-function sendCommand (command) {
-  const client = new EC2Client({ region: process.env.AWS_REGION })
-  return client.send(command)
+const { EC2Client, CreateImageCommand } = require("@aws-sdk/client-ec2");
+function sendCommand(command) {
+  const client = new EC2Client({ region: process.env.AWS_REGION });
+  return client.send(command);
 }
 
-createImage('<instanceid>', 'hamsterImage')
-  .then(() => console.log('Complete'))
+createImage("<instanceid>", "hamsterImage").then(() => console.log("Complete"));
 
-async function createImage (seedInstanceId, imageName) {
-  // TODO: Implement AMI creation
+async function createImage(seedInstanceId, imageName) {
+  const params = {
+    InstanceId: seedInstanceId,
+    Name: imageName,
+  };
+  const command = new CreateImageCommand(params);
+  sendCommand(command);
 }
--- scripts/03/manage-ec2-instance.js
+++ scripts/03/manage-ec2-instance.js
@@ -1,18 +1,30 @@
 // Imports
-// TODO: Import the ec2 client
+const {
+  EC2Client,
+  TerminateInstancesCommand,
+  DescribeInstancesCommand,
+} = require("@aws-sdk/client-ec2");
 
-function sendCommand (command) {
-  const client = new EC2Client({ region: process.env.AWS_REGION })
-  return client.send(command)
+function sendCommand(command) {
+  const client = new EC2Client({ region: process.env.AWS_REGION });
+  return client.send(command);
 }
 
-async function listInstances () {
-  // TODO: List instances using DescribeInstancesCommand
+async function listInstances() {
+  const command = new DescribeInstancesCommand({});
+  const data = await sendCommand(command);
+  return data.Reservations.reduce((i, r) => {
+    return i.concat(r.Instances);
+  }, []);
 }
 
-async function terminateInstance (instanceId) {
-  // TODO: Terminate an instance with a given instanceId
+async function terminateInstance(instanceId) {
+  const params = {
+    InstanceIds: [instanceId],
+  };
+  const command = new TerminateInstancesCommand(params);
+  return sendCommand(command);
 }
 
-listInstances().then(console.log)
+listInstances().then(console.log);
 // terminateInstance().then(console.log)
